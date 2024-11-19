4cb7a5e2d990c7f77d6e0eac7c889d6adf756bb2
TroteaTeodor <teodor.trotea@gmail.com>
2024-11-19 14:25:18 +0100

    Added piece generation based on index and ability to display them!

--- Tetris/src/main/java/Tetris/src/main/java/com/example/Main.java
+++ Tetris/src/main/java/Tetris/src/main/java/com/example/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        Pieces p = new Pieces(2,3);
+        p.showPiece();
     }
 }
--- Tetris/src/main/java/Tetris/src/main/java/com/example/Pieces.java
+++ Tetris/src/main/java/Tetris/src/main/java/com/example/Pieces.java
@@ -1,19 +1,38 @@
 package Tetris.src.main.java.com.example;
 
+import java.util.Arrays;
+
 public class Pieces {
     private int filler; //The shape is made out of this number
-    private int[][] pieces;
-
-    public Pieces(int filler, int[][] pieces) {
+    private int[][][] shapes = {
+            {{0,1,0},{1,1,1}}, // T - 0
+            {{1,1,1}}, // I - 1
+            {{1,1},{1,1}}, // O - 2
+            {{0,1,1},{1,1,0}}, // Z - 3
+            {{1,1,0},{0,1,1}}, // S - 4
+            {{1,0},{1,0},{1,1}}, // L - 5
+            {{0,1},{0,1},{1,1}} // J - 6
+    };
+    private int[][] piece;
+    public Pieces(int filler, int shapeNumber) {
         this.filler = filler;
-        pieces = new int[pieces.length][pieces[0].length];
-        for (int i=0; i<pieces.length; i++) {
-            for (int j=0; j<pieces[i].length; j++) {
-                if (pieces[i][j] == 1)
-                    this.pieces[i][j] = filler;
+        piece = new int[shapes[shapeNumber].length][shapes[shapeNumber][0].length];
+        for (int i = 0; i < shapes[shapeNumber].length; i++) {
+            for (int j = 0; j < shapes[shapeNumber][i].length; j++) {
+                if (shapes[shapeNumber][i][j] == 1) {
+                    piece[i][j] = filler;
+                }
                 else
-                    this.pieces[i][j] = 0;
+                    piece[i][j] = 0;
             }
+        }
+    }
+    public void showPiece() {
+        for (int i = 0; i < piece.length; i++) {
+            for (int j = 0; j < piece[i].length; j++) {
+                System.out.print(piece[i][j] + " ");
+            }
+            System.out.println("");
         }
     }
 }
