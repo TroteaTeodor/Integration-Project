ed254826769f05bbd40a5053f19be82f4fda1b1d
TroteaTeodor <teodor.trotea@gmail.com>
2024-11-19 14:36:23 +0100

    V1.0

--- Tetris/src/main/java/Tetris/src/main/java/com/example/Main.java
+++ Tetris/src/main/java/Tetris/src/main/java/com/example/Main.java
@@ -1,11 +1,16 @@
 package Tetris.src.main.java.com.example;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
-        Pieces p = new Pieces(2,3);
-        p.showPiece();
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter the filler and the shape index: ");
+            Pieces p = new Pieces(sc.nextInt(), sc.nextInt());
+            p.showPiece();
+        }
     }
 }
