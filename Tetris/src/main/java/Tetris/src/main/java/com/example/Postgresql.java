cc80a3df0d1f88e08aa18e45a3aff798b3838ca9
TroteaTeodor <teodor.trotea@gmail.com>
2024-12-02 13:14:40 +0100

    added postgresql connection

--- Tetris/src/main/java/Tetris/src/main/java/com/example/Postgresql.java
+++ Tetris/src/main/java/Tetris/src/main/java/com/example/Postgresql.java
@@ -6,11 +6,13 @@ import java.sql.SQLException;
 
 public class Postgresql {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        String jdbcUrl = "jdbc:postgresql://localhost:5432/database_name";
-        String username = "username";
-        String password = "password";
+        String jdbcUrl = "jdbc:postgresql://localhost:5432/ascii29";
+        String username = "postgres";
+        String password = "Student_1234";
         Class.forName("org.postgresql.Driver");
         Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+        //connection.execute();
+        System.out.println(connection.getClientInfo());
         connection.close();
     }
 }
