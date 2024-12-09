cdb363b68e116ba5f475c878000aceb85572fb25
TroteaTeodor <teodor.trotea@gmail.com>
2024-12-09 15:02:36 +0100

    updated game

--- Tetris/setup.sql
+++ Tetris/setup.sql
@@ -1,6 +1,8 @@
 -- Active: 1733749329420@@127.0.0.1@5432@ascii29@public
 -- Create the players table
 -- Create the players table if it doesn't already exist
+DROP Table if EXISTS players, games;
+
 CREATE TABLE IF NOT EXISTS players (
     id SERIAL PRIMARY KEY,
     player_name VARCHAR(50) NOT NULL UNIQUE,
@@ -19,4 +21,5 @@ CREATE TABLE IF NOT EXISTS games (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-select * from games
+select * from games;
+SELECT * from players;
