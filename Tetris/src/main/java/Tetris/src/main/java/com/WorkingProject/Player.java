6ff447dbdeb989fa822727080286bbcbf71730e8
TroteaTeodor <teodor.trotea@gmail.com>
2024-12-22 15:07:39 +0100

    final version

--- Tetris/src/main/java/Tetris/src/main/java/com/WorkingProject/Player.java
+++ Tetris/src/main/java/Tetris/src/main/java/com/WorkingProject/Player.java
@@ -1,8 +1,7 @@
 package Tetris.src.main.java.com.WorkingProject;
 
 public class Player {
-    private int playerId; // Store the player's ID (this is what will be used to link the player to their
-                          // game data)
+    private int playerId; // Store the player's ID (this is what will be used to link the player to their game data)
     private String playerName; // Store the player's name
     private int score = 0;
     private int energyPoints = 5;
